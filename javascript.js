23ba99d48ea421be07f6b4335ae5158746ef5980
Gaurav Polai <polai.gaurav@gmail,com>
2023-04-18 13:44:41 +0530

    Creation Of Git

---
+++ javascript.js
@@ -0,0 +1,18 @@
+const x= document.getElementById('show'),
+      y= document.getElementById('show1'),
+      z=document.getElementById('signinshow')
+
+function toggleMenu(){
+    y.classList.toggle('hidden');
+
+}
+
+// function main(){
+//     z.classList.remove('hidden');
+// }
+
+
+
+
+
+
